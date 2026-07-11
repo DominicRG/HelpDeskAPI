c9de49b87853b4b58c51c5110392b815cde55e02
Dominic <romandominic30@gmail.com>
2026-07-11 10:37:22 -0500

    AreaController, get añadido

---
+++ HelDesk.API/Controllers/AreaController.cs
@@ -0,0 +1,26 @@
+﻿using HelpDesk.Application.Area.Services.Interfaces;
+using HelpDesk.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelDesk.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AreaController : ControllerBase
+    {
+        private readonly IAreaService _service;
+
+        public AreaController(IAreaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var areas = await _service.GetAllAsync();
+
+            return Ok(ApiResponseFactory.Success(areas, "Áreas obtenidas correctamente"));
+        }
+    }
+}
