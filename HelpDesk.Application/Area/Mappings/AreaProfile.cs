7efd49009a98eaac76f0ad2b467f0c9aab74c433
Dominic Roman Guerrero <uaf474@pronis.gob.pe>
2026-07-17 15:55:52 -0500

    Autommaper part1

---
+++ HelpDesk.Application/Area/Mappings/AreaProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Application.Area.Mappings
+{
+    public class AreaProfile : Profile
+    {
+    }
+}
