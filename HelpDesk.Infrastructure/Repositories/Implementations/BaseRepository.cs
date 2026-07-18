01b41204f426c098a673e3ebf6f8982071bd8494
uaf474 <uaf474@pronis.gob.pe>
2026-07-18 15:56:19 -0500

    Añadiendo baseRepo

---
+++ HelpDesk.Infrastructure/Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using HelpDesk.Domain.Entities;
+using HelpDesk.Infrastructure.Persistence;
+using HelpDesk.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HelpDesk.Infrastructure.Repositories.Implementations
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public virtual void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _context.Set<T>().Update(entity);   
+        }
+    }
+}
---
+++ HelpDesk.Infrastructure/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using HelpDesk.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Infrastructure.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
